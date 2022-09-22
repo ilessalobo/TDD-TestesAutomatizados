ea7b6c65a2b078fbf4f166ba5ab9a074a654d2d6
Lobo <78517083+ilessalobo@users.noreply.github.com>
2022-09-22 10:22:39 -0400

    Update Funcionario.java
    Método para uso do arredondamento

--- Testes Unitarios/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ Testes Unitarios/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -29,7 +29,12 @@ public class Funcionario {
 	}
 
 	public void reajustarSalario(BigDecimal reajuste) {
-		this.salario = this.salario.add(reajuste).setScale(2, RoundingMode.HALF_UP);
+		this.salario = this.salario.add(reajuste);
+		arredondarSalario();
+	}
+
+	private void arredondarSalario() {
+		this.salario = this.salario.setScale(2, RoundingMode.HALF_UP);
 
 	}
 
